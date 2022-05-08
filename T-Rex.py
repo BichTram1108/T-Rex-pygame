4beb8a7916385f77b67779a61e98022b63e0d521
BichTram1108 <92030292+BichTram1108@users.noreply.github.com>
2022-05-08 20:43:33 +0700

    delete obstacle + update code

--- T-Rex.py
+++ T-Rex.py
@@ -5,8 +5,53 @@ import random
 from hinhanh import *
 from Dinosaur import *
 from Cloud import *
-from Obstacle import *
 
+
+pygame.init()
+
+class Obstacle:
+    
+    def __init__(self, image, type):
+        self.image = image
+        self.type = type
+        self.rect = self.image[self.type].get_rect()
+        self.rect.x = SCREEN_WIDTH
+
+    def update(self):
+        global game_speed, obstacles
+        game_speed = 14
+        obstacles = []
+        self.rect.x -= game_speed
+        if self.rect.x < -self.rect.width:
+            obstacles.pop()
+
+    def draw(self, SCREEN):
+        SCREEN.blit(self.image[self.type], self.rect)
+
+class SmallCactus (Obstacle):
+    def __init__(self, image):
+        self.type = random.randint(0, 2)
+        super().__init__(image, self.type)
+        self.rect.y = 325
+
+class LargeCactus (Obstacle):
+    def __init__(self, image):
+        self.type = random.randint(0, 2)
+        super().__init__(image, self.type)
+        self.rect.y = 300
+
+class Bird (Obstacle):
+    def __init__(self, image):
+        self.type = 0
+        super().__init__(image, self.type)
+        self.rect.y = 250
+        self.index = 0
+
+    def draw(self, SCREEN):
+        if self.index >= 9:
+            self.index = 0
+        SCREEN.blit(self.image[self.index//5], self.rect)
+        self.index += 1
 
 
 def main():
