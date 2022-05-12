bcea8ef24cdcee8948d9aa47849dcfdf647525c0
BichTram1108 <92030292+BichTram1108@users.noreply.github.com>
2022-05-12 18:35:49 +0700

    update code nâng cấp nhân vật + picture

--- Dinosaur3.py
+++ Dinosaur3.py
@@ -6,18 +6,15 @@ from hinhanh import *
 
 pygame.init()
 
-class Dinosaur:
+class Dinosaur3:
     X_POS = 80
-    Y_POS = 350
-    Y_POS_DUCK = 440
+    Y_POS = 320
     JUMP_VEL = 8.5
 
     def __init__(self):
-        self.duck_img = DUCKING
-        self.run_img = RUNNING
-        self.jump_img = JUMPING
+        self.run_img = RUNNING_3
+        self.jump_img = JUMPING_3
 
-        self.dino_duck = False
         self.dino_run = True
         self.dino_jump = False
 
@@ -29,8 +26,6 @@ class Dinosaur:
         self.dino_rect.y = self.Y_POS
 
     def update(self, userInput):
-        if self.dino_duck:
-            self.duck()
         if self.dino_run:
             self.run()
         if self.dino_jump:
@@ -43,22 +38,11 @@ class Dinosaur:
             self.dino_jump = True
             self.dino_duck = False
             self.dino_run = False
-        elif userInput[pygame.K_DOWN] and not self.dino_jump:
-            self.dino_duck = True
-            self.dino_jump = False
-            self.dino_run = False
         elif not (self.dino_jump or userInput[pygame.K_DOWN]):
             self.dino_run = True
             self.dino_jump = False
             self.dino_duck = False 
-
-    def duck(self):
-        self.image = self.duck_img[self.step_index // 5]
-        self.dino_rect = self.image.get_rect()
-        self.dino_rect.x = self.X_POS
-        self.dino_rect.y = self.Y_POS_DUCK
-        self.step_index += 1
-
+            
     def run(self):
         self.image = self.run_img[self.step_index // 5]
         self.dino_rect = self.image.get_rect()
